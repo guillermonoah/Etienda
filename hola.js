d7bce8f4a9133793c718004f0df0384add9b0a54
guille <guillermocarrasco2a@gmail.com>
2024-09-10 12:56:21 -0300

    primer commit

---
+++ hola.js
@@ -0,0 +1,21 @@
+// hola modificado del local huawei mejorado
+
+
+// aqui se mstraran las intalaciondes de paquetes que no se suben a git por el gitignore
+/*
+en tienda/admin :
+
+npm i exceljs
+npm i file-saver
+npm i jquery
+npm i @auth0/angular-jwt
+*/
+/*
+en tienda/tienda :
+npm install @ng-bootstrap/ng-bootstrap
+npm i @angular/localize
+npm install @popperjs/core --save
+video 48 no existe el archivo polyfills.ts--sin cambios no se hizo el import'@angular/localize/init'
+
+
+*/
